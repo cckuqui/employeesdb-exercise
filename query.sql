a9f5d09594320da7ed11a196e146f53ff1a1e2eb
Cris B <cristina.bardan@gmail.com>
2020-07-12 18:47:16 -0500

    organized files

---
+++ query.sql
@@ -0,0 +1,191 @@
+﻿-- Create Tables from EDR
+CREATE TABLE "employees" (
+    "emp_no" int NOT NULL,
+    "birth_date" date NOT NULL,
+    "first_name" varchar NOT NULL,
+    "last_name" varchar NOT NULL,
+    "gender" varchar NOT NULL,
+    "hire_date" date NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar NOT NULL,
+    "dept_name" varchar NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar NOT NULL,
+    "emp_no" int NOT NULL,
+    "from_date" date NOT NULL,
+    "to_date" date NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int NOT NULL,
+    "dept_no" varchar NOT NULL,
+    "from_date" date NOT NULL,
+    "to_date" date NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int NOT NULL,
+    "salary" int NOT NULL,
+    "from_date" date NOT NULL,
+    "to_date" date NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_no" int NOT NULL,
+    "title" varchar NOT NULL,
+    "from_date" date NOT NULL,
+    "to_date" date NOT NULL
+);
+
+-- Add foreing keys to tables
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- Question 1 = List the following details of each employee: employee number, last name, 
+-- first name, gender, and salary.
+select 
+   e.emp_no, 
+   e.last_name, 
+   e.first_name, 
+   e.gender, 
+   s.salary
+from employees e
+left join salaries s 
+on e.emp_no = s.emp_no
+order by e.emp_no 
+
+-- Question 2 = List employees who were hired in 1986.
+select emp_no, first_name, last_name 
+from employees
+where extract(year from hire_date) = 1986;
+
+-- Question 3 = List the manager of each department with the following information: 
+--  department number, department name, the manager's employee number, last name,
+--  first name, and start and end employment dates.
+select 
+   dm.dept_no, 
+   d.dept_name, 
+   dm.emp_no, 
+   e.last_name, 
+   e.first_name, 
+   dm.from_date, 
+   dm.to_date
+from dept_manager dm
+left join departments d
+on dm.dept_no = d.dept_no
+left join employees e
+on dm.emp_no = e.emp_no;
+
+-- Question 4 = List the department of each employee with the following information:
+--  employee number, last name, first name, and department name.
+select 
+   ede.emp_no, 
+   ede.last_name, 
+   ede.first_name, 
+   ede.dept_name
+from 
+   (select
+      e.emp_no, 
+      max(last_name) last_name, 
+      max(first_name) first_name, 
+      max(dept_name) dept_name,
+      max(to_date)
+   from employees e
+   left join dept_emp de
+   on e.emp_no = de.emp_no
+   left join departments d
+   on de.dept_no = d.dept_no
+   group by e.emp_no) ede;
+   
+select 
+   emp_no,
+   max(dept_no),
+   max(to_date) 
+from dept_emp
+group by emp_no;
+
+-- Question 5 = List all employees whose first name is "Hercules" and last names 
+-- begin with "B."
+select * from employees
+where first_name = 'Hercules' 
+and last_name like 'B%';
+
+-- Question 6 = List all employees in the Sales department, including their employee number,
+--  last name, first name, and department name.
+select 
+   ede.emp_no, 
+   ede.last_name, 
+   ede.first_name, 
+   ede.dept_name
+from 
+   (select
+      e.emp_no, 
+      max(last_name) last_name, 
+      max(first_name) first_name, 
+      max(dept_name) dept_name,
+      max(to_date)
+   from employees e
+   left join dept_emp de
+   on e.emp_no = de.emp_no
+   left join departments d
+   on de.dept_no = d.dept_no
+   group by e.emp_no) ede
+where ede.dept_name='Sales';
+
+-- Question 7 = List all employees in the Sales and Development departments, including
+--  their employee number, last name, first name, and department name.
+select 
+   ede.emp_no, 
+   ede.last_name, 
+   ede.first_name, 
+   ede.dept_name
+from 
+   (select
+      e.emp_no, 
+      max(last_name) last_name, 
+      max(first_name) first_name, 
+      max(dept_name) dept_name,
+      max(to_date)
+   from employees e
+   left join dept_emp de
+   on e.emp_no = de.emp_no
+   left join departments d
+   on de.dept_no = d.dept_no
+   group by e.emp_no) ede
+where ede.dept_name='Sales'
+or ede.dept_name='Development'; --missing last employement
+
+-- Question 8 = In descending order, list the frequency count of employee last names, i.e.,
+--  how many employees share each last name.
+select 
+   last_name, 
+   count(last_name)
+from employees
+group by last_name
+order by 
+   count(last_name) desc;
